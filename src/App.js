e9da2cfaf957f536c8a10fc13b362ad5a9adf7cb
chaitukumar15 <cheymadakasira@gmail.com>
2022-11-21 11:22:25 +0530

    changes 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HomePage from "./components/start.js"
+// import Shop from "./components/items.js"
+
+function App() {
+  return (
+    <>
+    <HomePage/>
+   
+    </>
+  );
+}
+
+export default App;
